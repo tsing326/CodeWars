438698ef05fdb62cb7e402a07ca70309366b79b2
tsing326 <104868092+tsing326@users.noreply.github.com>
2022-06-01 15:05:42 -0400

    Create SetAlarm.js

---
+++ SetAlarm.js
@@ -0,0 +1,4 @@
+//Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+function setAlarm(employed, vacation){
+  return employed && !vacation;
+}
